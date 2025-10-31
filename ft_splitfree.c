725bbac50cbccacc0659828abf7d728070d7c4b4
Pedro Lourenço <pedrolourencoemail@gmail.com>
2025-10-31 13:06:28 +0000

    added safeguard for splitfree

--- ft_splitfree.c
+++ ft_splitfree.c
@@ -16,8 +16,14 @@ void	ft_splitfree(char **split)
 {
 	int	i;
 
+	if (!split)
+		return ;
 	i = 0;
 	while (split[i])
-		free(split[i++]);
+	{
+		if (split[i])
+			free(split[i]);
+		i++;
+	}
 	free(split);
 }
